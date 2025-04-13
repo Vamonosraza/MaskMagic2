e313b5b89afc71897a343aa1f3766eff8d6ba9e2
Vamonosraza <martinezjessy393@gmail.com>
2025-04-13 14:30:27 -0700

    Initial Commit

---
+++ MaskMagic2/MaskMagic2App.swift
@@ -0,0 +1,17 @@
+//
+//  MaskMagic2App.swift
+//  MaskMagic2
+//
+//  Created by Jessy  Martinez  on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MaskMagic2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MaskMagic2Tests/MaskMagic2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MaskMagic2Tests.swift
+//  MaskMagic2Tests
+//
+//  Created by Jessy  Martinez  on 4/13/25.
+//
+
+import Testing
+@testable import MaskMagic2
+
+struct MaskMagic2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
