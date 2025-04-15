db0753d1b8b566ae765e0018e1440bc2dfa17d41
Vamonosraza <martinezjessy393@gmail.com>
2025-04-15 12:08:29 -0700

    Added a configuration template

---
+++ MaskMagic2/ConfigurationTemplate.swift
@@ -0,0 +1,23 @@
+//
+//  ConfigurationTemplate.swift
+//  MaskMagic2
+//
+//  Created by Jessy  Martinez  on 4/15/25.
+//
+
+import Foundation
+
+//struct Configuration {
+//    // Removed API key - Now secured in GoogleCloud Function
+//        
+//        // API endpoints
+//        static let imageGenerationEndpoint = "YOUR_API_ENDPOINT_HERE"
+//        
+//        // App theme colors
+//        struct Colors {
+//            static let primary = "35155D"
+//            static let secondary = "512B81"
+//            static let text = "4477CE"
+//            static let accent = "8CABFF"
+//        }
+//}
