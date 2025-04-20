f1721268a887425f2d398873a4de72616dca5319
Vamonosraza <martinezjessy393@gmail.com>
2025-04-20 16:31:08 -0700

    pushing configuration

---
+++ MaskMagic2/Configuration.swift
@@ -0,0 +1,23 @@
+//
+//  Configuration.swift
+//  MaskMagic2
+//
+//  Created by Jessy  Martinez  on 4/13/25.
+//
+
+import Foundation
+
+struct Configuration {
+    
+        
+        // API endpoints
+        static let imageGenerationEndpoint = "https://generateimage-1090278563288.us-central1.run.app"
+        
+        // App theme colors
+        struct Colors {
+            static let primary = "35155D"
+            static let secondary = "512B81"
+            static let text = "4477CE"
+            static let accent = "8CABFF"
+        }
+}
